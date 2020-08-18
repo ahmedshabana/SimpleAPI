ff5fd6f127d6587c7e527e2d5b1efea0db7049ec
Ahmed Shabana <ashabana@cairo.aas.com.sa>
2020-08-18 14:27:19 +0200

    Initial Commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        SimpleAPI.Controllers.WeatherForecastController Weather1 =new WeatherForecastController();
+        [Fact]
+        public void Test1()
+        {
+
+        }
+         [Fact]
+        public void GetWeatherData()
+        {
+            
+            var returnValue = Weather1.Get();
+
+             Assert.Equal("Ahmed Shabana" ,returnValue.ToString());
+
+
+
+        }
+    }
+}
